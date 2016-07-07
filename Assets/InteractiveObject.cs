b7bb39889d51120a8f23be3ef817f8544c96e799
msf567 <msf567@gmail.com>
2016-07-07 16:58:00 -0400

    init commit

---
+++ Assets/InteractiveObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Collider))]
+public class InteractiveObject : MonoBehaviour
+{
+	public bool interactedWith = false;
+	public TextAsset InkFile;
+
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	void OnInteract ()
+	{
+		
+	}
+}
---
+++ Assets/ObjectScanner.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectScanner : MonoBehaviour
+{
+	private bool zoomed = false;
+	public float scanProgress = 0;
+	private float scanSpeed = 0.5f;
+	private Camera cam;
+
+	void Awake ()
+	{
+		cam = GetComponent<Camera> ();
+	}
+
+	void Update ()
+	{
+		zoomed = Input.GetButton ("Fire2");
+
+		if (zoomed && LookingAtInteractiveObject ()) {
+			ProgressScan ();
+		} else {
+			ResetScan ();
+		}
+	}
+
+	bool LookingAtInteractiveObject ()
+	{
+		RaycastHit hit;
+		Ray ray = new Ray (cam.transform.position, cam.transform.forward);
+
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.transform.gameObject.GetComponent<InteractiveObject> ()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		return false;
+	}
+
+	void ProgressScan ()
+	{
+		if (scanProgress >= 1.0f)
+			FinishScan ();
+		
+		scanProgress += scanSpeed * Time.deltaTime;
+		Debug.Log (scanProgress);
+	}
+
+	void FinishScan ()
+	{
+		Debug.Log ("Scan Completed!");
+		ResetScan ();
+	}
+
+	void ResetScan ()
+	{
+		scanProgress = 0;
+	}
+}
